75d9975a43da4a43596175b98d52acf6d0b74218
skyorbit0 <skyorbit@126.com>
2018-12-05 14:58:54 +0800

    Updates

---
+++ precache-manifest.6d8252c0b9eb5aa16a3917e01c110b18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94da852d20de59b4b493",
+    "url": "/jianshu-react/static/js/runtime~main.94da852d.js"
+  },
+  {
+    "revision": "21a278ae60c5da7dc7d2",
+    "url": "/jianshu-react/static/js/main.21a278ae.chunk.js"
+  },
+  {
+    "revision": "e0a448ddaefe943017ff",
+    "url": "/jianshu-react/static/js/2.e0a448dd.chunk.js"
+  },
+  {
+    "revision": "75a0ff0c9a0842d1fcc0",
+    "url": "/jianshu-react/static/js/1.75a0ff0c.chunk.js"
+  },
+  {
+    "revision": "21a278ae60c5da7dc7d2",
+    "url": "/jianshu-react/static/css/main.f6fc463c.chunk.css"
+  },
+  {
+    "revision": "fc6011683cadb4f3a6c003cada435ed2",
+    "url": "/jianshu-react/index.html"
+  }
+];
